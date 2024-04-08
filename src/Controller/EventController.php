e6c5d9d1c0b5ad20bc07ad57e7363dd47d783e60
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2024-04-08 16:23:44 +0200

    fosuserbundle

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,17 +3,30 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\User;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EventController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(EventRepository $repository): Response
+    public function index(EventRepository $repository, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
     {
         $events = $repository->findAll();
+
+//        $user = new User();
+//        $user->setEmail('siracuse.harichandra@gmail.com')
+//            ->setName('Harichandra')
+//            ->setFirstname('SIRACUSE')
+//            ->setPassword($hasher->hashPassword($user, 'azed'))
+//            ->setRoles(['ROLE_ADMIN']);
+//
+//        $em->persist($user);
+//        $em->flush();
         return $this->render('index.html.twig', [
             'events' => $events,
         ]);
