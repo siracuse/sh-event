3d0cc3d06c6f52cbc9190f013e3225dde4f9b3a2
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2025-03-17 12:00:24 +0100

    date format and btn retour

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
