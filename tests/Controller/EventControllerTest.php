799970ed3fd51545e2d115f0cf549853a7c7fe9c
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2026-03-13 18:01:24 +0100

    edit = test d'application

--- tests/Controller/EventControllerTest.php
+++ tests/Controller/EventControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class EventControllerTest extends WebTestCase
@@ -14,12 +15,18 @@ class EventControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
     }
 
-    // public function testRegister200()
-    // {
-    //     $client = static::createClient();
-    //     $client->request('GET', '/event'); 
+    public function testSomething(): void
+    {
+        $client = static::createClient();
 
-    //     $this->assertResponseIsSuccessful();
-    // }
+        $eventRepository = $client->getContainer()->get('doctrine')->getRepository(Event::class);
+        $event = $eventRepository->findOneBy([]); // [] = aucun critère → premier trouvé
+
+        $this->assertNotNull($event, 'Aucun événement trouvé dans la base de test');
+
+        $eventId = $event->getId();
+        $client->request('GET', '/event/' . $eventId);
+        $this->assertResponseIsSuccessful();
+    }
 
 }
